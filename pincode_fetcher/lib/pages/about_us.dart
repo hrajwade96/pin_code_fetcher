5f299e423a75dc8784934fd97b2e27fcf819a769
hrajwade96 <hrishikeshrajwade96@gmail.com>
2023-10-24 22:05:41 +0530

    added app drawer, about us

---
+++ pincode_fetcher/lib/pages/about_us.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AboutUs extends StatelessWidget {
+  const AboutUs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About us'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
--- pincode_fetcher/lib/pages/landing_page.dart
+++ pincode_fetcher/lib/pages/landing_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pincode_fetcher/stores/view_port_store.dart';
 import '../stores/location_store.dart';
 import '../widgets/views/desktop.dart';
+import 'about_us.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -12,6 +13,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String s1 = '';
+  int _selectedIndex = 0;
 
   // This controller will store the value of the search bar
   final TextEditingController _searchController = TextEditingController();
@@ -22,6 +24,55 @@ class _MyHomePageState extends State<MyHomePage> {
     final viewport = ViewportStore();
 
     return Scaffold(
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Pin Code fetcher'),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(0);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.account_circle),
+              title: const Text('About us'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(1);
+                // Then close the drawer
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.help),
+              title: const Text('Help'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(2);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: const Text('Pincode Fetcher'),
       ),
@@ -29,5 +80,17 @@ class _MyHomePageState extends State<MyHomePage> {
           child:
               DesktopView(searchController: _searchController, store: store)),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      if (index == 1) {
+        debugPrint('index is 1');
+        Navigator.pop(context);
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const AboutUs()));
+      }
+    });
   }
 }
