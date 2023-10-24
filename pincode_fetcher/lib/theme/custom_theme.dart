d6af336cc79137a8f7579898ab617864c5c68f8c
hrajwade96 <hrishikeshrajwade96@gmail.com>
2023-10-24 22:07:58 +0530

    added custom styles

---
+++ pincode_fetcher/lib/theme/custom_theme.dart
@@ -0,0 +1,11 @@
+// style.dart
+import 'package:flutter/material.dart';
+
+const TextStyle kTableHeaderTextStyle = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+
+Color kTableHeaderColor = Colors.grey.shade200;
+
+// You can add more styles and themes as required for your app
--- pincode_fetcher/lib/widgets/output.dart
+++ pincode_fetcher/lib/widgets/output.dart
@@ -1,76 +1,72 @@
+// Import the style file
 import 'package:flutter/material.dart';
+import '../theme/custom_theme.dart';
 import 'selectable_text.dart' as st;
 import '../model/location_details.dart';
 
 class GenerateAddressOutput extends StatefulWidget {
-  const GenerateAddressOutput({super.key, required this.data});
+  const GenerateAddressOutput({Key? key, required this.data}) : super(key: key);
   final LocationData data;
+
   @override
-  DataTableExample createState() => DataTableExample();
+  _GenerateAddressOutputState createState() => _GenerateAddressOutputState();
 }
 
-class DataTableExample extends State<GenerateAddressOutput> {
-  // final List<DataRow> dataList= widget.data;
+class _GenerateAddressOutputState extends State<GenerateAddressOutput> {
+  late List<DataRow> _dataRows;
 
   @override
-  Widget build(BuildContext context) {
-    final ScrollController controller1 = ScrollController();
-    final ScrollController controller2 = ScrollController();
-
-    return Scrollbar(
-      child: SingleChildScrollView(
-        controller: controller2,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          controller: controller1,
-          child:
-              // const Center(
-              //     child: SizedBox(
-              //   child: Text(
-              //     'Location Details',
-              //     style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              //   ),
-              // )),
-              DataTable(
-            columns: const [
-              DataColumn(
-                  label: Text('Pincode',
-                      style: TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.bold))),
-              DataColumn(
-                  label: Text('State',
-                      style: TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.bold))),
-              DataColumn(
-                  label: Text('City',
-                      style: TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.bold))),
-              DataColumn(
-                  label: Text('Area',
-                      style: TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.bold))),
-              DataColumn(
-                  label: Text('Locality',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)))
-            ],
-            rows: [
-              if (widget.data.results != null)
-                for (var i in widget.data.results!) _buildTableRow(i)
-            ],
-          ),
-        ),
-      ),
-    );
+  void initState() {
+    super.initState();
+    _dataRows = widget.data.results?.map(_buildTableRow).toList() ?? [];
   }
 
   DataRow _buildTableRow(Results result) {
     return DataRow(cells: [
-      DataCell((st.Selectable(result.postalCode ?? ''))),
+      DataCell(st.Selectable(result.postalCode ?? '')),
       DataCell(st.Selectable(result.adminName1 ?? '')),
       DataCell(st.Selectable(result.adminName2 ?? '')),
       DataCell(st.Selectable(result.adminName3 ?? '')),
       DataCell(st.Selectable(result.placeName ?? '')),
     ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minWidth: constraints.maxWidth),
+              child: DataTable(
+                showBottomBorder: true,
+                headingRowColor: MaterialStateProperty.all(kTableHeaderColor),
+                columns: const <DataColumn>[
+                  DataColumn(
+                    label: Text('Pincode', style: kTableHeaderTextStyle),
+                  ),
+                  DataColumn(
+                    label: Text('State', style: kTableHeaderTextStyle),
+                  ),
+                  DataColumn(
+                    label: Text('City', style: kTableHeaderTextStyle),
+                  ),
+                  DataColumn(
+                    label: Text('Area', style: kTableHeaderTextStyle),
+                  ),
+                  DataColumn(
+                    label: Text('Locality', style: kTableHeaderTextStyle),
+                  ),
+                ],
+                rows: _dataRows,
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
