6c6c0aa5419348aa5798c670eda910d8ec9bf660
hrajwade96 <hrishikeshrajwade96@gmail.com>
2023-10-25 22:39:56 +0530

    UI enhancements

--- pincode_fetcher/lib/widgets/views/desktop.dart
+++ pincode_fetcher/lib/widgets/views/desktop.dart
@@ -10,137 +10,166 @@ class DesktopView extends StatelessWidget {
   double? height;
   double? width;
   DesktopView({
-    super.key,
+    Key? key,
     required TextEditingController searchController,
     required this.store,
     this.height,
     this.width,
-  }) : _searchController = searchController;
+  }) : _searchController = searchController, super(key: key);
 
   final TextEditingController _searchController;
   final LocationStore store;
 
   @override
   Widget build(BuildContext context) {
-    height = MediaQuery.sizeOf(context).height;
-    width = MediaQuery.sizeOf(context).width;
-    return Padding(
-      padding: EdgeInsets.fromLTRB(
-          0.1 * width!.toDouble(), 0, ((0.2 * width!.toDouble())), 0),
-      child: Column(children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 32.0),
-          child: Container(
-            // Add padding around the search bar
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            // Use a Material design search bar
-            child: TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                hintText: 'Search...',
-                // Add a clear button to the search bar
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.clear),
-                  onPressed: () => _searchController.clear(),
-                ),
-                // Add a search icon or button to the search bar
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: IconButton(
-                    icon: const Icon(Icons.search),
-                    onPressed: () {
-                      // Perform the search here
-                      store.count = 0;
-                      store.searchQuery = _searchController.text;
-                      store.fetchAddress(_searchController.text);
-                    },
-                  ),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-              ),
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        height: height,
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(
+            colors: [Color(0xFFB2EBF2), Color(0xFF0288D1)], // Updated colors
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+          // Overlaying with a pattern. This is just an example, and you might need to fetch a pattern of your choice.
+          image: DecorationImage(
+            image: const AssetImage('path_to_your_pattern.png'),  // Replace with the path to your pattern image
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.1),  // Adjust this value to make the pattern more or less visible
+              BlendMode.dstATop,
             ),
           ),
         ),
-        const SizedBox(
-          height: 50,
-        ),
-        Observer(builder: (context) {
-          final data = store.addressData.value?.value;
-          if (store.addressData.value?.status == FutureStatus.pending) {
-            debugPrint(
-                'Loading Widget.${DateTime.now().millisecondsSinceEpoch}');
-            return Stack(children: [
-              SizedBox(
-                child: Center(
-                  child: Padding(
-                    padding: EdgeInsets.all(
-                        MediaQuery.sizeOf(context).height * 0.25),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(
-                              MediaQuery.sizeOf(context).aspectRatio * 2.45),
-                          child: LoadingAnimationWidget.discreteCircle(
-                            color: Colors.red,
-                            size: 50,
-                          ),
-                        ),
-                        if (MediaQuery.sizeOf(context).width > 1024)
-                          Padding(
-                            padding: EdgeInsets.all(
-                                MediaQuery.sizeOf(context).aspectRatio * 3.45),
-                            child: const Text('Loading...',
-                                style: TextStyle(fontSize: 35)),
-                          ),
-                      ],
+
+        child: SingleChildScrollView( // Add SingleChildScrollView
+          padding: EdgeInsets.symmetric(
+            horizontal: 0.15 * width!, // Adjust the horizontal padding symmetrically
+            vertical: 0.15 * height!, // You can also adjust the vertical padding as needed
+          ),
+          child: Column(children: [
+            // Stylized Search Bar
+            // Stylized Search Bar
+            // Stylized Search Bar
+            Center(
+              child: Container(
+                width: 0.6 * width!,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 2,
+                      blurRadius: 4,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Center(  // Added this to center-align the TextField
+                  child: TextField(
+                    controller: _searchController,
+                    style: const TextStyle(fontSize: 18),
+                    decoration: InputDecoration(
+                      hintText: 'Enter your pincode/state/city/area!',
+                      suffixIcon: IconButton(
+                        iconSize: 20,  // Adjust as per requirement
+                        icon: const Icon(Icons.clear),
+                        onPressed: () => _searchController.clear(),
+                      ),
+                      prefixIcon: IconButton(
+                        iconSize: 20,  // Adjust as per requirement
+                        icon: const Icon(Icons.search),
+                        onPressed: () {
+                          store.count = 0;
+                          store.searchQuery = _searchController.text;
+                          store.fetchAddress(_searchController.text);
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),  // Further adjusted for better alignment
+                      isDense: true,  // Added this to adjust vertical alignment
+                      alignLabelWithHint: true,  // Helpful if using labelText
                     ),
                   ),
                 ),
               ),
-            ]);
-          }
+            ),
 
-          debugPrint('data is $data');
-          if (store.addressData.value?.status == FutureStatus.fulfilled) {
-            debugPrint(
-                'Finished Widget.${DateTime.now().millisecondsSinceEpoch}');
-            if (data != null && data.results!.isNotEmpty) {
-              store.count = 0;
-              return Column(
-                children: [
-                  const Center(
-                      child: SizedBox(
-                    child: Text(
-                      'Location Details',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
-                  )),
-                  SizedBox(
-                    height: 600,
-                    child: GenerateAddressOutput(data: data),
-                  ),
-                ],
-              );
-            } else {
-              if (store.count < 5) {
-                store.fetchAddress(_searchController.text);
-              } else {
-                return const Center(
-                  child: Text(
-                    'No results found!',
-                    style: TextStyle(fontSize: 20),
+            const SizedBox(
+              height: 50,
+            ),
+            Observer(builder: (context) {
+              final data = store.addressData.value?.value;
+              if (store.addressData.value?.status == FutureStatus.pending) {
+                return Center( // Wrap with Center widget to align the content
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,  // To vertically center-align the items
+                    children: [
+                      LoadingAnimationWidget.discreteCircle(
+                        color: Colors.red,
+                        size: 50,
+                      ),
+                      const SizedBox(
+                        height: 20,  // Increase this value to add more space
+                      ),
+                      const Text('Loading...', style: TextStyle(fontSize: 35)),
+                    ],
                   ),
                 );
               }
+              if (store.addressData.value?.status == FutureStatus.fulfilled) {
+                if (data != null && data.results!.isNotEmpty) {
+                  store.count = 0;
+                  return Column(
+                    children: [
+                      const Center(
+                        child: Text(
+                          'Location Details',
+                          style: TextStyle(
+                            fontSize: 28, // Increased font size
+                            fontWeight: FontWeight.bold,
+                            shadows: [ // Added shadow for a bit of depth
+                              Shadow(
+                                offset: Offset(1.0, 1.0),
+                                blurRadius: 2.0,
+                                color: Color.fromARGB(125, 0, 0, 255),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),  // Added space between the text and the table
+                      SizedBox(
+                        height: 600,
+                        child: GenerateAddressOutput(data: data),
+                      ),
+                    ],
+                  );
+                } else {
+                  if (store.count < 5) {
+                    store.fetchAddress(_searchController.text);
+                  } else {
+                    return const Center(
+                      child: Text(
+                        'No results found!',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    );
+                  }
+                }
+              }
               return Container();
-            }
-          }
-          return Container();
-        }),
-      ]),
+            }),
+          ]),
+        ),
+      ),
     );
   }
 }
